b639f58238ca0a91a2193590f59e221f67020260
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2024-11-21 04:44:46 +0000

    More detailed retrieve transactions SQL script

--- html/sql/transaction_retrieve.sql
+++ html/sql/transaction_retrieve.sql
@@ -1,3 +1,15 @@
 -- Get all transactions.
-SELECT transaction_id, transaction_timestamp, partner_id, shipment_id, employee_id, location_id
+SELECT transactions.transaction_id,
+       transaction_timestamp,
+       partner_name,
+       shipment_id,
+       employee_name,
+       transactions.location_id,
+       GROUP_CONCAT(CONCAT(product_name, " x ", transaction_product_quantity) SEPARATOR ", ") AS products,
+       CONCAT("$", SUM(transaction_product_price * transaction_product_quantity)) AS amount_transacted
   FROM transactions
+       LEFT JOIN partners ON partners.partner_id = transactions.partner_id
+       LEFT JOIN employees ON employees.employee_id = transactions.employee_id
+       LEFT JOIN transaction_products ON transaction_products.transaction_id = transactions.transaction_id
+       LEFT JOIN products ON products.product_id = transaction_products.product_id
+ GROUP BY transaction_id
