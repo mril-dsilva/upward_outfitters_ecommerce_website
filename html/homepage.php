0f96d610d11d5f54f505d370750341437e8ad171
ozarikar <omkar.zarikar@centre.edu>
2024-12-05 20:24:58 +0000

    added a print

--- html/homepage.php
+++ html/homepage.php
@@ -2,6 +2,12 @@
  <?php 
 require 'navbar.php'; 
 ?>
+<?php
+    // Show all errors from the PHP interpreter.
+    ini_set('display_errors', 1);    
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+?>
 
 <!DOCTYPE html>
 <html>
